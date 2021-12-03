8734d006e6aa812e76a55d85cd7069f5a3dcba3a
JaredMillsap <jmillsap23@gmail.com>
2021-12-03 10:06:40 -0600

    selection statements

---
+++ Selection Statements 1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Selection_Statements_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //// Assigning Variables and Writing them out to the console.
+
+            //// int= variable Type     x=Variable Name     Y = Variable Name  10=Value  ;=(End of statement)
+            int x = 10;
+            int y = 101;
+
+            int answer = x / y;
+
+            Console.WriteLine(answer);
+
+
+
+            string firstBorn = ("Preston");
+            string secondBorn = ("Jared");
+            string thirdBorn = ("Jordan");
+            string fourthBorn = ("Maegan");
+           string fifthBorn = ("Makenzie");
+
+            Console.WriteLine($"{firstBorn},{secondBorn},{thirdBorn},{fourthBorn},{fifthBorn} are the children of the Millsap Family.");
+
+
+             //Char= Variable Type   middleInitial= W *notice char ' ' are single quaotation marks that are always used wit the char variable type
+            char middleInitial = 'W';
+
+            Console.WriteLine($" My full name is Jared {middleInitial} Millsap");
+
+
+            //************** SELECTION STATEMENTS*********************
+
+
+            
+
+
+
+
+
+
+        }
+            
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ User Inputs Selection Statemen/User Inputs Selection Statemen/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace User_Inputs_Selection_Statemen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool condition = true;
+            int favNumber = 200;
+            string input = Console.ReadLine();
+            int userguess = int.Parse(input);
+
+            Console.WriteLine("What is my favorite?");
+           
+            if (userguess == favNumber)
+
+            {
+                Console.WriteLine("The variable is set to true.");
+            }
+             else
+            {
+                Console.WriteLine("The variable is set to true.");
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            //Console.WriteLine("What is your name?");
+            //var UserInput = Console.ReadLine();
+
+
+
+            //Console.WriteLine("What school do you attend?");
+            //_ = Console.ReadLine();
+
+
+            //Console.WriteLine("What is your major?");
+            //_ = Console.ReadLine();
+
+
+            //Console.WriteLine("When do you graduate?");
+            //_ = Console.ReadLine();
+
+            //Console.WriteLine("What are your plans after graduation?");
+            //_ = Console.ReadLine();
+
+            //Console.WriteLine(" After you get a job?");
+            //_ = Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
